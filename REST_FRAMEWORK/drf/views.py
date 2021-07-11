5674f72db83485753ee503f59aa1a69f92093c5a
Sunny <sunnysaraff11@gmail.com>
2021-07-12 00:13:22 +0530

    using mixins

--- REST_FRAMEWORK/drf/views.py
+++ REST_FRAMEWORK/drf/views.py
@@ -15,3 +15,18 @@ class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.Gener
         return self.create(request, *args, **kwargs)
 
 
+class SnippetDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
