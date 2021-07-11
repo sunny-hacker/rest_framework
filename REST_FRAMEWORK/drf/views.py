94fdf555f8be7c2e12a364ec6a21dadcc371bd99
Sunny <sunnysaraff11@gmail.com>
2021-07-11 18:30:21 +0530

    views files updated

--- REST_FRAMEWORK/drf/views.py
+++ REST_FRAMEWORK/drf/views.py
@@ -1,12 +1,13 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 from .models import Snippet
 from .serializers import SnippetSerializer
 
 
 @api_view(['GET', 'POST'])
-def snippet_list(request):
+def snippet_list(request, format=None):
     """
     List all code snippets, or create a new snippet.
     """
@@ -24,7 +25,7 @@ def snippet_list(request):
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk):
+def snippet_detail(request, pk, format=None):
     """
     Retrieve, update or delete a code snippet.
     """
