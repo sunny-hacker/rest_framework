bedfb2b199961a8653f4305b10679287a37d0f17
Sunny <sunnysaraff11@gmail.com>
2021-07-11 12:49:49 +0530

    Project models updated

--- REST_FRAMEWORK/REST_FRAMEWORK/settings.py
+++ REST_FRAMEWORK/REST_FRAMEWORK/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
+    'drf.apps.DrfConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
